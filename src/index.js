f1ed9530d6e0cfc70e87d52e99177822b7b79b72
Sergey Donyk <doncarlos@i.ua>
2023-03-19 23:45:51 +0200

    render

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
+// import Notiflix from 'notiflix';
+// import axios, { Axios } from 'axios';
+// // import { fetchImg } from './fetchImg.js';
+
+// const API_URL = 'https://pixabay.com/api/';
+// const YOUR_API_KEY = '34476830 - b52e87f2018fae84058c602d8';
+
 import Notiflix from 'notiflix';
-import axios, { Axios } from 'axios';
-// import { fetchImg } from './fetchImg.js';
+import axios from 'axios';
 
 const API_URL = 'https://pixabay.com/api/';
-// const YOUR_API_KEY = '34476830 - b52e87f2018fae84058c602d8';
 
 const refs = {
   searching: document.querySelector('[name="searchQuery"]'),
   form: document.querySelector('.search-form'),
+  query: document.querySelector('input'),
   gallery: document.querySelector('.gallery'),
 };
-console.log(refs.searching);
-console.log(refs.form);
-console.log(refs.gallery);
-refs.searching.addEventListener('submit', handleSearch);
+
+refs.form.addEventListener('submit', handleSearch);
+
 async function handleSearch(event) {
   event.preventDefault();
-
-  refs.searching.value.trim();
+  const query = refs.query.value.trim();
+  refs.query.value.trim();
   if (query === '') {
     Notiflix.Notify.warning('Please enter a search query.');
     return;
   }
 
-  const apiKey = '34476830 - b52e87f2018fae84058c602d8';
-  const url = `${API_URL}?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const apiKey = '34476830-b52e87f2018fae84058c602d8';
+
+  const url = `${API_URL}?key=${apiKey}&q=${refs.query.value}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   try {
     const response = await axios.get(url);
@@ -46,7 +52,22 @@ async function handleSearch(event) {
       downloads: hit.downloads,
     }));
 
-    // Відобразити знайдені зображення
+    const html = images
+      .map(
+        image => `
+      <div class="photo-card">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+          <p class="info-item"><b>Views:</b> ${image.views}</p>
+          <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+          <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+        </div>
+      </div>
+    `
+      )
+      .join('');
+    refs.gallery.insertAdjacentHTML('beforeend', html);
   } catch (error) {
     console.error(error);
     Notiflix.Notify.failure('Something went wrong. Please try again later.');
